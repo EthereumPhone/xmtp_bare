7d6077a2c0fe42654997fa16cbe4d771a29f8115
Markus Haas <haas.markus@protonmail.com>
2022-09-10 15:58:31 +0200

    change

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
-npx browserify init.js > /home/markus/AndroidStudioProjects/TestXMTPSdk/xmtp-android-sdk/src/main/res/raw/init.js
-npx browserify sendmessage.js > /home/markus/AndroidStudioProjects/TestXMTPSdk/xmtp-android-sdk/src/main/res/raw/sendmessage.js
-npx browserify getpeeraccounts.js > /home/markus/AndroidStudioProjects/TestXMTPSdk/xmtp-android-sdk/src/main/res/raw/getpeeraccounts.js
-npx browserify getmessages.js > /home/markus/AndroidStudioProjects/TestXMTPSdk/xmtp-android-sdk/src/main/res/raw/getmessages.js
+npx browserify init.js > /home/markus/AndroidStudioProjects/xmtp-android-sdk/xmtp-android-sdk/src/main/res/raw/init.js
+npx browserify sendmessage.js > /home/markus/AndroidStudioProjects/xmtp-android-sdk/xmtp-android-sdk/src/main/res/raw/sendmessage.js
+npx browserify getpeeraccounts.js > /home/markus/AndroidStudioProjects/xmtp-android-sdk/xmtp-android-sdk/src/main/res/raw/getpeeraccounts.js
+npx browserify getmessages.js > /home/markus/AndroidStudioProjects/xmtp-android-sdk/xmtp-android-sdk/src/main/res/raw/getmessages.js
+npx browserify listenmessages.js > /home/markus/AndroidStudioProjects/xmtp-android-sdk/xmtp-android-sdk/src/main/res/raw/listenmessages.js
